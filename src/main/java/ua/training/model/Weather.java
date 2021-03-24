476cb2132497f1cc71936da71363b298378c96bf
Xitaymin <y.barashenko@gmail.com>
2021-03-24 19:54:18 +0200

    Added date field in Weather,added contructor with default date.

--- src/main/java/ua/training/model/Weather.java
+++ src/main/java/ua/training/model/Weather.java
@@ -1,5 +1,7 @@
 package ua.training.model;
 
+import java.time.LocalDate;
+
 public class Weather {
     private Direction windDirection;
     private int windSpeed;
@@ -7,15 +9,19 @@ public class Weather {
     private int maxTemperature;
     private Cloudiness cloudiness;
     private int precipitation;
+    private LocalDate date;
 
-    public Weather(Direction windDirection,int windSpeed,int minTemperature, int maxTemperature,Cloudiness cloudiness,int precipitation) {
+    public Weather(Direction windDirection,int windSpeed,int minTemperature, int maxTemperature,Cloudiness cloudiness,int precipitation, LocalDate date) {
     this.windDirection = windDirection;
     this.windSpeed = windSpeed;
     this.minTemperature = minTemperature;
     this.maxTemperature = maxTemperature;
     this.cloudiness = cloudiness;
     this.precipitation = precipitation;
+    this.date = date;
     }
 
-
+    public Weather(Direction windDirection,int windSpeed,int minTemperature, int maxTemperature,Cloudiness cloudiness,int precipitation) {
+        this(windDirection,windSpeed,minTemperature,maxTemperature,cloudiness,precipitation,LocalDate.now());
+    }
 }
